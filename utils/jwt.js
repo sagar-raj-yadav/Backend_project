f6226ac23328ada1ffa41d65ec4cc2f7ce69f555
sagar raj yadav <115137591+sagar-raj-yadav@users.noreply.github.com>
2024-09-13 03:31:26 +0530

    Update jwt.js

--- utils/jwt.js
+++ utils/jwt.js
@@ -4,5 +4,8 @@ const generateAccessToken = (user) => {
   return jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_ACCESS_EXPIRATION });
 };
 
+const generateRefreshToken = (user) => {
+  return jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_REFRESH_EXPIRATION });
+};
 
 module.exports = { generateAccessToken, generateRefreshToken };
