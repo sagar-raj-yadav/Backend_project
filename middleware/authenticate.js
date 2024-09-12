4f63fb0677ff281f5c9254caa2cf1e9956c607fe
sagar raj yadav <115137591+sagar-raj-yadav@users.noreply.github.com>
2024-09-13 03:30:26 +0530

    Update authenticate.js

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -4,7 +4,9 @@ const User = require('../models/User');
 const authenticate = async (req, res, next) => {
   const token = req.header('Authorization').replace('Bearer ', '');
 
- 
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided, authorization denied' });
+  }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
