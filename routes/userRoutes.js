a49d54fb8683a45e6b6dafe8c2c2d4493862c8b0
sagar raj yadav <115137591+sagar-raj-yadav@users.noreply.github.com>
2024-09-13 03:31:01 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,9 +4,6 @@ const authenticate = require('../middleware/authenticate');
 
 const router = express.Router();
 
-router.get('/', authenticate, getAllUsers);
-router.get('/:id', authenticate, getUserById);
-router.put('/:id', authenticate, updateUser);
 router.delete('/:id', authenticate, deleteUser);
 
 module.exports = router;
