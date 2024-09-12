f71d38ac7b7f2643bac2049ba7692d04acb09f00
sagar raj yadav <115137591+sagar-raj-yadav@users.noreply.github.com>
2024-09-13 03:30:43 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const { register, login, refreshToken } = require('../controllers/authController');
 
-const router = express.Router();
-
-router.post('/register', register);
-router.post('/login', login);
 router.post('/refresh-token', refreshToken);
 
 module.exports = router;
