0da6e7def873d6425aba34f6268147a8a41cb7fc
sagar raj yadav <115137591+sagar-raj-yadav@users.noreply.github.com>
2024-09-13 03:31:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,10 +18,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-
 
 app.get(('/'),(req,res)=>{
   res.send("Hello world")
