329b411d2bd11a25ff01d52c38b465fc807125b8
sagar raj yadav <115137591+sagar-raj-yadav@users.noreply.github.com>
2024-09-13 03:29:57 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,9 +7,7 @@ exports.getAllUsers = async (req, res) => {
 
 exports.getUserById = async (req, res) => {
   const user = await User.findById(req.params.id);
-  if (!user) {
-    return res.status(404).json({ message: 'User not found' });
-  }
+
   res.json(user);
 };
 
